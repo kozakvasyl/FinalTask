6a4449c18df746f90483e1252067da45b0850304
kozakvasyl <koretsua@gmail.com>
2020-02-01 23:42:22 +0200

    writed function scrolled to reviews and addForm

--- FinalTask/bonfire/web/js/mainScripts.js
+++ FinalTask/bonfire/web/js/mainScripts.js
@@ -4,13 +4,17 @@ define([
     'domReady!'
     ], function ($) {
 
-    //show addReview form
-    $('.review_hiden').click(function () {
-        $(".block-content").toggleClass("show-review-form");
+    //hide header panel
+    $('.panel.wrapper').find('.panel.header').prepend('<span class="hide-header-panel"></span>');
+    $('.hide-header-panel').click(function () {
+        $('.panel.wrapper').hide();
     });
 
+
+
+
     //scroll page to reviews
-    $('.product-reviews-summary').find('.action.view').click(function() {
+    /* $('.product-reviews-summary').find('.action.view').click(function() {
         $('.block.review-list').toggleClass('show-element');
         $('.show-element').prepend('<span class="hide-element"></span>');
 
@@ -25,7 +29,36 @@ define([
             }, 1000);
             $('.hide-element').remove();
         });
+    }); */
+
+    var scrollTo = function(element) {
+        element.addClass('show-element').prepend('<span class="hide-element"></span>');
+
+        $('html, body').animate({
+            scrollTop: element.offset().top
+        }, 1000);
+
+        $('.hide-element').click(function() {
+            element.removeClass('show-element');
+            $('html, body').animate({
+                scrollTop: $('.header.content').offset().top
+            }, 1000);
+            $('.hide-element').remove();
+        });
+    };
+
+    var reviews = $('.block.review-list');
+    $('.product-reviews-summary').find('.action.view').off('click').on('click', function () {
+        scrollTo(reviews);
     });
+    var formAddReviews = $('.review-form');
+    $('.product-reviews-summary').find('.add-review-buton').off('click').on('click', function () {
+        scrollTo(formAddReviews);
+    });
+
+
+
+
 
     //select plugin
     $('.select-city, .select-phone').select2({
