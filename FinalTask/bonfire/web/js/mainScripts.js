98157c50ee050d577ffbe70590018146cdd2a036
kozakvasyl <koretsua@gmail.com>
2020-02-01 18:57:36 +0200

    added script to hide review form and scroll to top page

--- FinalTask/bonfire/web/js/mainScripts.js
+++ FinalTask/bonfire/web/js/mainScripts.js
@@ -11,18 +11,20 @@ define([
 
     //scroll page to reviews
     $('.product-reviews-summary').find('.action.view').click(function() {
-        $('.block.review-list').toggleClass("show-element");
-        $('html, body').animate({
-          scrollTop: $(".block.review-list").offset().top
-        }, 1000)
-    });
+        $('.block.review-list').toggleClass('show-element');
+        $('.show-element').prepend('<span class="hide-element"></span>');
 
-    //scroll page to show addReview form
-    $('.action.add').click(function() {
-        $(".block-content").toggleClass("show-review-form");
         $('html, body').animate({
-            scrollTop: $(".show-review-form").offset().top
-          }, 1000)
+            scrollTop: $('.block.review-list').offset().top
+        }, 1000);
+
+        $('.hide-element').click(function() {
+            $('.block.review-list').removeClass('show-element');
+            $('html, body').animate({
+                scrollTop: $('.header.content').offset().top
+            }, 1000);
+            $('.hide-element').remove();
+        });
     });
 
     //select plugin
